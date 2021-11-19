d774a6d8173d134c9da479bac584b1fafa5c498c
fouziOukacha <gf_oukacha@esi.dz>
2021-11-19 16:25:38 +0100

    npm install

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import React, {useState, useEffect} from 'react';
-import {useSelector, useDispatch} from "react-redux";
-import { dispatchAction } from './utils/general/dispatch.util';
-import userActions from './redux/user/actions';
-import { isAuthenticated } from './utils/general/auth.util';
+import {useDispatch} from "react-redux";
 import Layout from './components/layout/Layout/Layout.js';
-import Drawer from './components/layout/Drawer/DrawerComponent.js';
-
-import cookie from 'js-cookie'
 
 
 function App() {
-	// const isLogged = JSON.parse(localStorage.getItem('isLogged'))
-
 	const user = JSON.parse(localStorage.getItem('user'))
-
-	const dispatch = useDispatch()
-
-
-	
-
-	// useEffect(() => {
-	// 	const isAuth = isAuthenticated()
-	// 	// if(isAuth){
-	// 	// 	dispatch(dispatchAction(userActions.SET_STATE,{isLogged:true}))
-	// 	// }else{
-	// 	// 	dispatch(dispatchAction(userActions.SET_STATE,{isLogged:false}))
-	// 	// }
-	// },[]);
 	return (	
 		<React.Fragment>
 			<Layout user={user}  />
